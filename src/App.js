bf035c7e351ba98fea7f084c7659a106e361930d
zainaab-jamal <zeinabrateb30@gmail.com>
2023-02-06 22:08:16 +0200

    Lab2 -Forms

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import React from "react";
+import './App.css'
+import LoginForm from './Components/LoginForm';
+// import LoginForm from './Components/LoginForm';
+import RegisterForm from './Components/register';
+
+
+function App(){
+    return ( 
+        <div>
+            
+        <LoginForm/>
+        <hr/>
+        <RegisterForm/>
+        
+
+        </div>
+        
+        
+    );
+}
+export default App
